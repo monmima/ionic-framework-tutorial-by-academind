ad2869331f6f2de20888fe80cdbb9c84a63dd4e7
monmima <monmima@protonmail.com>
2021-03-07 19:43:16 -0500

    not at 50.42 in tutorial

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,9 +1,12 @@
 <template>
     <base-layout pageTitle="All Memories">
         <ion-list>
-            <ion-item router-link="/memories/1">A trip to the mountains</ion-item>
-            <ion-item>Surfing the sea</ion-item>
-            <ion-item>Good eating</ion-item>
+            <ion-item v-for="memory in memories" :router-link="`/memories/${memory.id}`" :key="memory.id">
+                <ion-thumbnail slot="start">
+                    <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+                </ion-thumbnail>
+                <ion-label>{{ memory.title }}</ion-label>
+            </ion-item>
         </ion-list>
     </base-layout>
 </template>
@@ -11,7 +14,10 @@
 <script>
 import {
     IonList,
-    IonItem
+    IonItem,
+    IonImg,
+    IonThumbnail,
+    IonLabel
 } from "@ionic/vue"
 
 import BaseLayout from '../components/base/BaseLayout.vue';
@@ -20,7 +26,34 @@ export default {
     components: {
         IonList,
         IonItem,
+        IonImg,
+        IonThumbnail,
+        IonLabel,
         BaseLayout
+    },
+    data() {
+        return {
+            memories: [
+                {
+                    id: "m1",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Castle_Mountain.jpg/270px-Castle_Mountain.jpg",
+                    title: "A trip to the mountains",
+                    description: "It was a really nice trip"
+                },
+                {
+                    id: "m2",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Mavericks_Surf_Contest_2010b.jpg/220px-Mavericks_Surf_Contest_2010b.jpg",
+                    title: "Surfing the sea",
+                    description: "Feeling the cool breeze"
+                },
+                {
+                    id: "m3",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/220px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
+                    title: "Good eating",
+                    description: "Really good."
+                }
+            ]
+        };
     }
 }
 </script>
